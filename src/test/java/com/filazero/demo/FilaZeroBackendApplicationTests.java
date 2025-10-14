f3a4f36ff20f18a26e8bfcaa5d4b7c736b1a4a24
Fer <desarrollowebfer@gmail.com>
2025-10-14 09:42:26 +0200

    fix: add changes in pom.xm.

---
+++ src/test/java/com/filazero/demo/FilaZeroBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.filazero.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class FilaZeroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
